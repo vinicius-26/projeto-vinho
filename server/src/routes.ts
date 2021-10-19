c2ae60e9bed8ba03a0e41efbf9d45e0891f66b86
Vinicius <vistephano123@gmail.com>
2021-10-19 12:47:11 -0300

    Criação das rotas de cadastro de clientes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,12 +3,14 @@ import express from 'express';
 const MoviesController = require ('./controllers/MoviesController')
 const UsersController = require ('./controllers/UsersController');
 const TanqueController = require ('./controllers/TanquesController');
+const ClienteController = require('./controllers/ClientesController')
 
 const routes = express.Router();
 
 const moviesController = new MoviesController();
 const usersController = new UsersController();
 const tanquesController = new TanqueController();
+const clientesController = new ClienteController();
 
 //rotas para buscar os filmes
 routes.get('/movies',moviesController.index);
@@ -25,6 +27,10 @@ routes.put('/editmovie',usersController.editWishList);
 //rota para cadastro de tanques
 routes.post('/addtanque',tanquesController.create);
 routes.get('/listtanques',tanquesController.index);
+
+//rota para cadastro de clientes
+routes.post('/addcliente',clientesController.create);
+routes.get('/listclientes',clientesController.index);
 
 
 
